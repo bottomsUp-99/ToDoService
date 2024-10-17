90525ce4ac10e3930a5fbc8957b6341f455bc7f8
leejuhwan <seungjoon0324@gmail.com>
2024-10-17 15:56:22 +0900

    component 세분화

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="todo">
+        <div class="todo__title">
+            <h1 class="todo__text">Todo List</h1>
+            <ul class="todo__tab">
+                <li class="todo__tab--active">전체</li>
+            <li>완료</li>
+            </ul>
+        </div>
+    </div>
+</template>
